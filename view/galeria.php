f85acad345cc0d986aeab2da7046117c46a7b636
allanAlves1904 <allanalves1904@gmail.com>
2018-03-31 00:18:09 -0300

    versao com a galeria

--- view/galeria.php
+++ view/galeria.php
@@ -5,11 +5,235 @@
 
 <!-- Segura conteudo -->
   <div class="principal_galeria centro_lr">
-    
+
     <div class="div_segura">
-
+      <div class="titulo_galeria titulo">
+        MOTOS
+      </div>
+      <div class="img_galeria">
+        <div class="segura_img">
+          <img src="pictures/galeria/moto_um.jpg" alt="Imagem da Galeria">
+        </div>
+        <div class="escondidinha">
+          <div class="parceiro_titulo conteudo ">
+              Parceiro Tal xxxxx
+          </div>
+          <div class="detalhe_img conteudo ">
+            dsgggrgregerherhrthrthrthrtnrtnregthgrwhjw5rj
+          </div>
+        </div>
+      </div>
+      <div class="img_galeria">
+        <div class="segura_img">
+          <img src="pictures/galeria/moto_dois.jpg" alt="Imagem da Galeria">
+        </div>
+        <div class="escondidinha">
+          <div class="parceiro_titulo conteudo ">
+              Parceiro Tal xxxxx
+          </div>
+          <div class="detalhe_img conteudo ">
+            dsgggrgregerherhrthrthrthrtnrtnregthgrwhjw5rj
+          </div>
+        </div>
+      </div>
+      <div class="img_galeria">
+        <div class="segura_img">
+          <img src="pictures/galeria/moto_tres.jpg" alt="Imagem da Galeria">
+        </div>
+        <div class="escondidinha">
+          <div class="parceiro_titulo conteudo ">
+              Parceiro Tal xxxxx
+          </div>
+          <div class="detalhe_img conteudo ">
+            dsgggrgregerherhrthrthrthrtnrtnregthgrwhjw5rj
+          </div>
+        </div>
+      </div>
+      <div class="img_galeria">
+        <div class="segura_img">
+          <img src="pictures/galeria/moto_quatro.jpg" alt="Imagem da Galeria">
+        </div>
+        <div class="escondidinha">
+          <div class="parceiro_titulo conteudo ">
+              Parceiro Tal xxxxx
+          </div>
+          <div class="detalhe_img conteudo ">
+            dsgggrgregerherhrthrthrthrtnrtnregthgrwhjw5rj
+          </div>
+        </div>
+      </div>
     </div>
-
+    <div class="div_segura">
+      <div class="titulo_galeria titulo">
+        SERVIÇOS
+      </div>
+      <div class="img_galeria">
+        <div class="segura_img">
+          <img src="pictures/galeria/pintura_carro.jpg" alt="Imagem da Galeria">
+        </div>
+        <div class="escondidinha">
+          <div class="parceiro_titulo conteudo ">
+              Parceiro Tal xxxxx
+          </div>
+          <div class="detalhe_img conteudo ">
+            dsgggrgregerherhrthrthrthrtnrtnregthgrwhjw5rj
+          </div>
+        </div>
+      </div>
+      <div class="img_galeria">
+        <div class="segura_img">
+          <img src="pictures/galeria/troca_oleo.jpg" alt="Imagem da Galeria">
+        </div>
+        <div class="escondidinha">
+          <div class="parceiro_titulo conteudo ">
+              Parceiro Tal xxxxx
+          </div>
+          <div class="detalhe_img conteudo ">
+            dsgggrgregerherhrthrthrthrtnrtnregthgrwhjw5rj
+          </div>
+        </div>
+      </div>
+      <div class="img_galeria">
+        <div class="segura_img">
+          <img src="pictures/galeria/auto_eletrica.jpg" alt="Imagem da Galeria">
+        </div>
+        <div class="escondidinha">
+          <div class="parceiro_titulo conteudo ">
+              Parceiro Tal xxxxx
+          </div>
+          <div class="detalhe_img conteudo ">
+            dsgggrgregerherhrthrthrthrtnrtnregthgrwhjw5rj
+          </div>
+        </div>
+      </div>
+      <div class="img_galeria">
+        <div class="segura_img">
+          <img src="pictures/galeria/polimento.jpg" alt="Imagem da Galeria">
+        </div>
+        <div class="escondidinha">
+          <div class="parceiro_titulo conteudo ">
+              Parceiro Tal xxxxx
+          </div>
+          <div class="detalhe_img conteudo ">
+            dsgggrgregerherhrthrthrthrtnrtnregthgrwhjw5rj
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="div_segura">
+      <div class="titulo_galeria titulo">
+        CARROS
+      </div>
+      <div class="img_galeria">
+        <div class="segura_img">
+          <img src="pictures/galeria/moto_um.jpg" alt="Imagem da Galeria">
+        </div>
+        <div class="escondidinha">
+          <div class="parceiro_titulo conteudo ">
+              Parceiro Tal xxxxx
+          </div>
+          <div class="detalhe_img conteudo ">
+            dsgggrgregerherhrthrthrthrtnrtnregthgrwhjw5rj
+          </div>
+        </div>
+      </div>
+      <div class="img_galeria">
+        <div class="segura_img">
+          <img src="pictures/galeria/moto_dois.jpg" alt="Imagem da Galeria">
+        </div>
+        <div class="escondidinha">
+          <div class="parceiro_titulo conteudo ">
+              Parceiro Tal xxxxx
+          </div>
+          <div class="detalhe_img conteudo ">
+            dsgggrgregerherhrthrthrthrtnrtnregthgrwhjw5rj
+          </div>
+        </div>
+      </div>
+      <div class="img_galeria">
+        <div class="segura_img">
+          <img src="pictures/galeria/moto_tres.jpg" alt="Imagem da Galeria">
+        </div>
+        <div class="escondidinha">
+          <div class="parceiro_titulo conteudo ">
+              Parceiro Tal xxxxx
+          </div>
+          <div class="detalhe_img conteudo ">
+            dsgggrgregerherhrthrthrthrtnrtnregthgrwhjw5rj
+          </div>
+        </div>
+      </div>
+      <div class="img_galeria">
+        <div class="segura_img">
+          <img src="pictures/galeria/moto_quatro.jpg" alt="Imagem da Galeria">
+        </div>
+        <div class="escondidinha">
+          <div class="parceiro_titulo conteudo ">
+              Parceiro Tal xxxxx
+          </div>
+          <div class="detalhe_img conteudo ">
+            dsgggrgregerherhrthrthrthrtnrtnregthgrwhjw5rj
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="div_segura">
+      <div class="titulo_galeria titulo">
+        PRODUTOS
+      </div>
+      <div class="img_galeria">
+        <div class="segura_img">
+          <img src="pictures/galeria/pintura_carro.jpg" alt="Imagem da Galeria">
+        </div>
+        <div class="escondidinha">
+          <div class="parceiro_titulo conteudo ">
+              Parceiro Tal xxxxx
+          </div>
+          <div class="detalhe_img conteudo ">
+            dsgggrgregerherhrthrthrthrtnrtnregthgrwhjw5rj
+          </div>
+        </div>
+      </div>
+      <div class="img_galeria">
+        <div class="segura_img">
+          <img src="pictures/galeria/troca_oleo.jpg" alt="Imagem da Galeria">
+        </div>
+        <div class="escondidinha">
+          <div class="parceiro_titulo conteudo ">
+              Parceiro Tal xxxxx
+          </div>
+          <div class="detalhe_img conteudo ">
+            dsgggrgregerherhrthrthrthrtnrtnregthgrwhjw5rj
+          </div>
+        </div>
+      </div>
+      <div class="img_galeria">
+        <div class="segura_img">
+          <img src="pictures/galeria/auto_eletrica.jpg" alt="Imagem da Galeria">
+        </div>
+        <div class="escondidinha">
+          <div class="parceiro_titulo conteudo ">
+              Parceiro Tal xxxxx
+          </div>
+          <div class="detalhe_img conteudo ">
+            dsgggrgregerherhrthrthrthrtnrtnregthgrwhjw5rj
+          </div>
+        </div>
+      </div>
+      <div class="img_galeria">
+        <div class="segura_img">
+          <img src="pictures/galeria/polimento.jpg" alt="Imagem da Galeria">
+        </div>
+        <div class="escondidinha">
+          <div class="parceiro_titulo conteudo ">
+              Parceiro Tal xxxxx
+          </div>
+          <div class="detalhe_img conteudo ">
+            dsgggrgregerherhrthrthrthrtnrtnregthgrwhjw5rj
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 
 
