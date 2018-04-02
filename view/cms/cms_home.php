374e613998cae5296732a4d2081ae174c9ac7b2e
Caique <caique_m_oliveira@outlook.com>
2018-04-01 23:20:48 -0300

    altera o design do menu lateral do cms junto a pagina da home

--- view/cms/cms_home.php
+++ view/cms/cms_home.php
@@ -3,20 +3,36 @@
   require_once('../component/cms_header.php');
 ?>
 
-    <div class="container_principal centro_lr">
+    <div class="container_principal sombra_preta_r_2 centro_lr">
 
       <?php
         // Importação do menu lateral
         require_once('../component/cms_menu_lateral.php');
       ?>
 
-      <div class="conteudo_central bg_cinza float_left">
+      <!-- Contáiner do conteúdo em si gerenciável do CMS -->
+      <div class="conteudo_central bg_branco float_left preenche_t_260">
 
-        <div class="fundo_preto preenche_t_260">
+        <!-- Contáiner da mensagem de saudação -->
+        <div class="mensgaem_saudacao conteudo align_center preenche_25">
 
-          <div class="mensgaem_saudacao conteudo txt_branco txt_verde_vivo txt_sombra_1x1x1_preto fs_40 align_center preenche_25">
-            Bem-vindo ao Sistema de Gerenciamneto de Conteúdo
+          <!-- Título da mensagem de saudação -->
+          <div class="titulo_saudacao fs_40">
+            Bem-vindo
           </div>
+
+          <!-- Preposiçãod da sudação -->
+          <div class="preposicao_saudacao fs_30 centro_lr margem_t_20">
+            ao
+          </div>
+
+          <!-- Subtítulo da saudação -->
+          <div class="subtitulo_saudacao fs_50 margem_t_20">
+            Sistema de Gerenciamento de Conteúdo
+          </div>
+
+          <!-- Divisior de conteúdo -->
+          <div class="divisor"></div>
 
         </div>
 
