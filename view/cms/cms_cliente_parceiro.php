968a16ebe3c16d77761562e913962f364180691d
IGOR BEZERRA RIOS <16254853@CFP127ALUNO.LOCAL>
2018-04-02 09:08:55 -0300

    cms cliente parceiro 50%

--- view/cms/cms_cliente_parceiro.php
+++ view/cms/cms_cliente_parceiro.php
@@ -9,31 +9,44 @@
   ?>
 
 <div class="container_principal_cliente">
-  <div class="container_conteudo">
+  <div class="container_segura_cadastro_cliente">
     <div class="container_segura_texto_imagem float_left">
-      <div class="container_texto_imagem align_center preenche_5 titulo">
+      <div class="texto_imagem align_center preenche_5 titulo">
         Foto Cadastro Cliente
       </div>
 
-      <div class="container_imagem_cliente centro_lr margem_t_20">
+      <div class="imagem_cliente centro_lr margem_t_20">
         <img src="" alt="">
       </div>
     </div>
 
     <div class="container_segura_texto_cadastro_cliente float_left">
       <div class="container_texto">
-        <div class="container_segura_texto_cadastro align_center preenche_5 titulo">
+        <div class="texto_cadastro align_center preenche_5 titulo">
           Cadastro Cliente
         </div>
-        <div class="container_edit_texto centro_lr">
-          <textarea name="txtarea" rows="5000" cols="40"></textarea>
+        <div class="edit_texto centro_lr margem_t_30">
+          <textarea class="no_resize" name="txtarea" rows="11" cols="40">
+
+          </textarea>
         </div>
       </div>
     </div>
 
-    <div class="container_segura_botao margem_t_150">
+    <div class="container_segura_botao margem_t_80">
       <div class="input_submit">
         <button type="button" name="btn_enviar">Enviar</button>
+      </div>
+    </div>
+
+    <div class="container_segura_cadastro_parceiro">
+      <div class="container_texto_imagem_parceiro align_center preenche_5 titulo">
+        <div class="texto_imagem align_left preenche_5 titulo">
+          Foto Cadastro Parceiro
+        </div>
+        <div class="imagem_cadastro_parceiro centro_lr">
+
+        </div>
       </div>
     </div>
 
