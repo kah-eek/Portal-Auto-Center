425ea7c9eea5d1ae187779ad5f18e98253451bbb
IgorRios27 <riosigor27@gmail.com>
2018-04-05 22:41:11 -0300

    cms cadastro sobre empresa completa

--- view/cms/cms_sobre_empresa.php
+++ view/cms/cms_sobre_empresa.php
@@ -8,27 +8,148 @@
     require_once('../component/cms_menu_lateral.php');
   ?>
   <div class="container_conteudo float_left bg_cinza">
-    <div class="container_missao bg_verde">
+
+    <!-- PARTE MISSAO -->
+
+    <!-- *************************************************************** -->
+
+    <div class="container_geral bg_cinza">
       <!-- TITULO -->
-      <div class="container_titulo bg_preto margem_t_10">
-        <div class="item_titulo align_center txt_branco titulo preenche_5">
+      <div class="container_titulo margem_t_5">
+        <div class="item_titulo align_center txt_sombra_1x1x1_preto negrito fs_20 titulo">
           Missão
         </div>
       </div>
 
       <!-- INFORMAÇÕES -->
-      <div class="container_infs centro_lr">
+      <div class="container_infs bg_branco borda_preta_1 sombra_preta_b_15">
         <!-- IMAGEM -->
         <div class="segura_imagem float_left">
-          <div class="item_img">
+          <div class="item_img margem_t_10 borda_preta_1 margem_t_5">
 
           </div>
         </div>
         <!-- TEXTAREA -->
         <div class="container_segura_textarea float_left">
-
+          <div class="segura_textarea">
+            <textarea placeholder="Digite Aqui!!!" name="name" rows="9" style="resize: none" cols="61"></textarea>
+          </div>
+        </div>
+        <!-- BOTÃO -->
+        <div class="container_salvar margem_t_40 float_left">
+          <div class="segura_salvar_foto margem_t_20 centro_lr">
+            <i class="material-icons" style="font-size:35px;">save</i>
+          </div>
         </div>
       </div>
     </div>
+
+    <!-- PARTE VISAO -->
+
+    <!-- *************************************************************** -->
+
+    <div class="container_geral bg_cinza">
+      <!-- TITULO -->
+      <div class="container_titulo margem_t_5">
+        <div class="item_titulo align_center txt_sombra_1x1x1_preto negrito fs_20 titulo">
+          Visão
+        </div>
+      </div>
+
+      <!-- INFORMAÇÕES -->
+      <div class="container_infs bg_branco borda_preta_1 sombra_preta_b_15">
+        <!-- IMAGEM -->
+        <div class="segura_imagem float_left">
+          <div class="item_img margem_t_10 borda_preta_1 margem_t_5">
+
+          </div>
+        </div>
+        <!-- TEXTAREA -->
+        <div class="container_segura_textarea float_left">
+          <div class="segura_textarea">
+            <textarea placeholder="Digite Aqui!!!" name="name" rows="9" style="resize: none" cols="61"></textarea>
+          </div>
+        </div>
+        <!-- BOTÃO -->
+        <div class="container_salvar margem_t_40 float_left">
+          <div class="segura_salvar_foto margem_t_20 centro_lr">
+            <i class="material-icons" style="font-size:35px;">save</i>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- PARTE VALORES -->
+
+    <!-- *************************************************************** -->
+
+    <div class="container_geral bg_cinza">
+      <!-- TITULO -->
+      <div class="container_titulo margem_t_5">
+        <div class="item_titulo align_center txt_sombra_1x1x1_preto negrito fs_20 titulo">
+          Valores
+        </div>
+      </div>
+
+      <!-- INFORMAÇÕES -->
+      <div class="container_infs bg_branco borda_preta_1 sombra_preta_b_15">
+        <!-- IMAGEM -->
+        <div class="segura_imagem float_left">
+          <div class="item_img margem_t_10 borda_preta_1 margem_t_5">
+
+          </div>
+        </div>
+        <!-- TEXTAREA -->
+        <div class="container_segura_textarea float_left">
+          <div class="segura_textarea">
+            <textarea placeholder="Digite Aqui!!!" name="name" rows="9" style="resize: none" cols="61"></textarea>
+          </div>
+        </div>
+        <!-- BOTÃO -->
+        <div class="container_salvar margem_t_40 float_left">
+          <div class="segura_salvar_foto margem_t_20 centro_lr">
+            <i class="material-icons" style="font-size:35px;">save</i>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- PARTE EMPRESA -->
+
+    <!-- *************************************************************** -->
+
+    <div class="container_geral bg_cinza">
+      <!-- TITULO -->
+      <div class="container_titulo margem_t_5">
+        <div class="item_titulo align_center  txt_sombra_1x1x1_preto negrito fs_20 titulo">
+          Empresa
+        </div>
+      </div>
+
+      <!-- INFORMAÇÕES -->
+      <div class="container_infs bg_branco borda_preta_1 sombra_preta_b_15">
+        <!-- IMAGEM -->
+        <div class="segura_imagem float_left">
+          <div class="item_img margem_t_10 borda_preta_1 margem_t_5">
+
+          </div>
+        </div>
+        <!-- TEXTAREA -->
+        <div class="container_segura_textarea float_left">
+          <div class="segura_textarea">
+            <textarea placeholder="Digite Aqui!!!" name="name" rows="9" style="resize: none" cols="61"></textarea>
+          </div>
+        </div>
+        <!-- BOTÃO -->
+        <div class="container_salvar margem_t_40 float_left">
+          <div class="segura_salvar_foto margem_t_20 centro_lr">
+            <i class="material-icons" style="font-size:35px;">save</i>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- FINALIZA -->
+
   </div>
 </div>
