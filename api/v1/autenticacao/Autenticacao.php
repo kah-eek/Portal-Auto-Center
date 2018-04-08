5cd3d41514bf398550c8381f61990aea3702dd1e
Caique <caique_m_oliveira@outlook.com>
2018-04-07 22:27:42 -0300

    adiciona o recurso de autenticacao na rota

---
+++ api/v1/autenticacao/Autenticacao.php
@@ -0,0 +1,53 @@
+<?php
+
+// Imports
+require_once('../database.php');
+// #############################
+
+class Autenticacao
+{
+  public $usuario;
+  public $senha;
+
+  // Construtor default
+  function __construct($usuario, $senha)
+  {
+    $this->usuario = $usuario;
+    $this->senha = $senha;
+  }
+
+  /**
+  * verifica as credencias do usuário
+  * @param $autenticacaoObj Objeto contendo os dados da autenticação do usuário a ser verificado
+  * @return true Credencial confirmada com sucesso
+  * @return false Credencial não existente ou ocorra alguma falha ao tentar buscar o registro no banco de dados
+  */
+  function login($autenticacaoObj)
+  {
+    // Instância de acesso ao db
+    $mySql = new MySql();
+
+    // Abre uma nova conexão com o db
+    $con = $mySql->getConnection();
+
+    $stmt = $con->prepare('SELECT COUNT(*) AS rows FROM tbl_usuario WHERE usuario=? AND senha=?');
+    $stmt->bindParam(1,$autenticacaoObj->usuario);
+    $stmt->bindParam(2,$autenticacaoObj->senha);
+    $stmt->execute();
+
+    // Fecha a conexão com o db
+    $con = null;
+
+    if ($stmt->rowCount() > 0) {
+      while($response = $stmt->fetch(PDO::FETCH_ASSOC))
+      {
+        return $response['rows'] > 0 ? true : false;
+      }
+    }
+
+    return false;
+  }
+
+}
+
+?>
