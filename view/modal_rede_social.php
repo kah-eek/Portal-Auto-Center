b22d4a9a6b6fe8405b9f0472350da6530f11a7a3
IGOR BEZERRA RIOS <16254853@CFP127ALUNO.LOCAL>
2018-04-11 11:22:50 -0300

    sincronizando projeto

--- view/modal_rede_social.php
+++ view/modal_rede_social.php
@@ -6,13 +6,64 @@
     <link rel="stylesheet" href="css/modal_rede_social.css">
   </head>
   <body>
-    <div class="container_menu_m float_left">
-      <div class="itens_menu">
+    <div class="container_segura_tudo">
+      <div class="container_menu_m float_left">
+        <div class="itens_menu">
 
+          <!-- IMAGEM DO MENU -->
+          <div class="container_img_menu_rs preenche_t_20">
+            <div class="item_img_menu_rs centro_lr">
+
+            </div>
+          </div>
+          <!-- PARTE EDITAR PERFIL -->
+          <div class="container_edit_perfil margem_t_20 centro_lr">
+            <div class="item_edit_perfil centro_lr">
+              <div class="img_configs float_left">
+                <img src="" alt="">
+              </div>
+              <div class="titulo_editar_perfil centro_lr">
+                <div class="item_editar_perfil preenche_t_10 align_center titulo">
+                  Editar Perfil
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- PARTE AGENDA -->
+          <div class="container_agenda margem_t_20 centro_lr">
+            <div class="item_agenda centro_lr">
+              <div class="img_agenda float_left">
+                <img src="" alt="">
+              </div>
+
+              <div class="titulo_agenda centro_lr">
+                <div class="item_agenda_titulo preenche_t_10 align_center titulo">
+                  Agenda
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
       </div>
-    </div>
-    <div class="container_lado_direito float_left">
+      <div class="container_lado_direito float_left">
+        <div class="container_conteudo_rs">
 
+          <div class="container_img_titulo_usuario float_left">
+            <div class="item_img_titulo_usuario">
+
+            </div>
+          </div>
+
+          <div class="container_titulo_usuario">
+            <div class="item_titulo_usuario">
+              Nome de Usuário
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </body>
 </html>
