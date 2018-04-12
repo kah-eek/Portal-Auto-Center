4201da52e799948bf7fbadf693fab38d3eed4234
IgorRios27 <riosigor27@gmail.com>
2018-04-12 16:37:52 -0300

    modal detalhes veiculos completa

--- view/modal_detalhes_veiculos.php
+++ view/modal_detalhes_veiculos.php
@@ -6,85 +6,100 @@
     <link rel="stylesheet" href="css/modal_detalhes_veiculos.css">
   </head>
   <body>
-    <div class="linha1_dv">
+    <div class="linha1_dv float_left bg_cinza">
 
-      <div class="container_img_detalhes_veiculos float_left">
-        <div class="item_img_detalhes_veiculos centro_lr">
+      <div class="container_img_detalhes_veiculos float_left margem_t_5">
+        <div class="item_img_detalhes_veiculos centro_lr borda_preta_1">
 
         </div>
       </div>
 
-      <div class="container_descricao_veiculo float_left">
-        <div class="item_descricao_veiculo centro_lr">
+      <div class="container_fotos_veiculos">
+        <div class="item_fotos_veiculos">
+          <div class="fotos_pequenas borda_preta_1 float_left margem_l_10 margem_t_10">
 
-          <div class="desc titulo fs_20 negrito preenche_t_20">
-            Nome Produto
           </div>
 
-          <div class="desc titulo fs_20 negrito preenche_t_20 margem_t_20">
-            Quantidade de Embalagem
+          <div class="fotos_pequenas borda_preta_1 float_left margem_l_10 margem_t_10">
+
           </div>
 
-          <div class="desc titulo fs_20 negrito preenche_t_20 margem_t_20">
-            Garantia
+          <div class="fotos_pequenas borda_preta_1 float_left margem_l_10 margem_t_10">
+
           </div>
 
-          <div class="desc titulo fs_20 negrito preenche_t_20 margem_t_20">
-            OBS
+          <div class="fotos_pequenas borda_preta_1 float_left margem_l_10 margem_t_10">
+
           </div>
+
+          <div class="fotos_pequenas borda_preta_1 float_left margem_l_10 margem_t_10">
+
+          </div>
+        </div>
+      </div>
+
+      <div class="container_descricao float_left">
+        <div class="titulo_desc titulo preenche_t_5 margem_l_5 fs_20 margem_t_20">
+          Descrição:
+        </div>
+        <div class="texto_desc justificado margem_l_5">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         </div>
       </div>
 
     </div>
     <!-- LINHA 2 -->
-    <div class="linha2_dv">
+    <div class="linha2_dv float_left bg_cinza">
 
-      <div class="container_fotos_veiculos">
-        <div class="item_fotos_veiculos">
-          <div class="fotos_pequenas float_left margem_l_10 margem_t_10">
+      <div class="container_descricao_veiculo float_left">
+        <div class="item_descricao_veiculo centro_lr">
 
+          <div class="desc titulo fs_18 negrito preenche_t_10 margem_t_5">
+            Categoria do Produto:
           </div>
 
-          <div class="fotos_pequenas float_left margem_l_10 margem_t_10">
-
+          <div class="desc titulo fs_18 negrito preenche_t_10 margem_t_5">
+            Nome do Produto:
           </div>
 
-          <div class="fotos_pequenas float_left margem_l_10 margem_t_10">
-
+          <div class="desc titulo fs_18 negrito preenche_t_10 margem_t_5">
+            Qtd embalagem:
           </div>
 
-          <div class="fotos_pequenas float_left margem_l_10 margem_t_10">
-
+          <div class="desc titulo fs_18 negrito preenche_t_10 margem_t_5">
+            Garantia:
           </div>
 
-          <div class="fotos_pequenas float_left margem_l_10 margem_t_10">
+          <div class="desc titulo fs_18 negrito preenche_t_10 margem_t_5">
+            Obs:
+          </div>
 
+          <div class="desc titulo fs_18 negrito preenche_t_10 margem_t_5">
+            Nome Parceiro:
+          </div>
+
+          <div class="desc titulo fs_18 negrito preenche_t_10 margem_t_5">
+            Endereço Parceiro:
+          </div>
+
+          <div class="desc titulo fs_18 negrito preenche_t_10 margem_t_5">
+            Telefone Parceiro:
+          </div>
+
+          <div class="desc titulo fs_18 negrito preenche_t_10 margem_t_5">
+            Celular Parceiro:
+          </div>
+
+          <div class="preco float_left margem_t_180">
+            <div class="item_preco float_right titulo align_center preenche_t_10 fs_20 borda_preta_1">
+              Preço
+            </div>
+          </div>
+
+          <div class="segura_bt_comprar float_left margem_t_50">
+            <input class="input_submit input_submit_dv float_right" type="submit" name="btn_comprar" value="Comprar">
           </div>
         </div>
-
-        <div class="preco float_left">
-          <div class="item_preco titulo align_center preenche_t_20 fs_20">
-            $$
-          </div>
-        </div>
-      </div>
-
-    </div>
-
-    <!-- LINHA3 -->
-    <div class="linha3_dv">
-
-      <div class="container_descricao float_left">
-        <div class="titulo_desc titulo align_center preenche_t_5 fs_20">
-          Descrição
-        </div>
-        <div class="texto_desc justificado borda_preta_1 margem_l_5">
-          dddddddd dfsds sdsds sasadsad dasdasd dsads sdsdssssssssssssssssssssss
-        </div>
-      </div>
-
-      <div class="segura_bt_ok float_left margem_t_215">
-        <input class="input_submit input_submit_dv float_right" type="submit" name="btn_ok" value="OK">
       </div>
     </div>
   </body>
