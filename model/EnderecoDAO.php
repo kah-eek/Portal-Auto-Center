5a6c1eb0045bdd3c6c8b248d7c0f38c30e7dfc1f
Caique <caique@mdftecnologia.com.br>
2018-04-13 14:37:42 -0300

    separa a controller da model - endereco

--- model/EnderecoDAO.php
+++ model/EnderecoDAO.php
@@ -4,30 +4,8 @@
 // @data 12/04/2018
 // @description Classe endereco
 
-class Endereco
+class EnderecoDAO
 {
-  public $idEstado;
-  public $idEndereco;
-  public $logradouro;
-  public $numero;
-  public $cidade;
-  public $cep;
-  public $bairro;
-  public $complemento;
-
-  // Construtor default
-  function __construct($numero,$cidade,$cep,$bairro,$complemento,$logradouro,$idEstado,$idEndereco)
-  {
-    $this->numero = $numero;
-    $this->cidade = $cidade;
-    $this->cep = $cep;
-    $this->bairro = $bairro;
-    $this->complemento = $complemento;
-    $this->logradouro = $logradouro;
-    $this->idEstado = $idEstado;
-    $this->idEndereco = $idEndereco;
-  }
-
   /**
   * Insere um novo endereço no banco de dados
   * @param $enderecoObj Objeto Endereco qual será inserido no banco de dados
