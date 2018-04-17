f7fdbe1b6e95c1f2fdc3783afbc7a5ccf5e873db
IgorRios27 <riosigor27@gmail.com>
2018-04-16 21:12:44 -0300

    veiculos cadastrados criada

---
+++ view/cms/cms_veiculos_cadastrados.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="../css/cms/cms_veiculos_cadastrados.css">
+    <link rel="stylesheet" href="../css/padroes.css">
+  </head>
+  <body>
+    <div class="container_conteudo_central_vc">
+      <!-- MENU LATERAL -->
+      <div class="container_menu_l_vc float_left borda_preta_1 margem_l_20">
+        <div class="container_img_menu_vc centro_lr borda_preta_1 margem_t_20">
+          <div class="item_img_menu_l_vc ">
+
+          </div>
+        </div>
+        <!-- NOME USUÁRIO -->
+        <div class="container_nome_usuario_vc margem_t_10">
+          <div class="item_nome_usuario_vc centro_lr align_center preenche_t_15 fs_18 negrito txt_branco">
+            Nome de Usuário
+          </div>
+        </div>
+      </div>
+      <!-- VEICULOS CADASTRADOS -->
+      <div class="container_cadastrados_vc float_left">
+        <div class="container_titulo_cadastrados centro_lr margem_t_20">
+          <div class="item_titulo_cadastrados centro_lr align_center preenche_t_35 fs_30 negrito txt_preto">
+            Veículos Cadastrados
+          </div>
+        </div>
+        <!-- CAIXA EDICAO -->
+        <div class="container_caixa_cadastrados float_left margem_t_40 margem_l_180">
+          <div class="container_item_caixa_cadastrados">
+            <div class="item_caixa_cadastrados float_left margem_l_15 align_center preenche_t_60 fs_20 negrito txt_preto">
+              Label
+            </div>
+            <div class="item_caixa_cadastrados float_left margem_l_15 align_center preenche_t_60 fs_20 negrito txt_preto">
+              Label
+            </div>
+            <div class="item_caixa_cadastrados float_left margem_l_15 align_center preenche_t_60 fs_20 negrito txt_preto">
+              Label
+            </div>
+            <div class="item_caixa_cadastrados float_right">
+              <i class="material-icons" style="font-size:50px;margin-top:25px;margin-left:20px;">delete_forever</i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
