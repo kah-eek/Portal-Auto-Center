fb01a105fdad2c916d0f03702030ee31a11fa9b4
IgorRios27 <riosigor27@gmail.com>
2018-04-18 17:46:35 -0300

    pagina agenda parceiro feita e inserida no link planos(tele inicial do parceiro)

---
+++ view/cms/parceiro/cms_agenda_parceiro.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="../../css/cms/cms_agenda_parceiro.css">
+    <link rel="stylesheet" href="../../css/padroes.css">
+  </head>
+  <body>
+    <div class="container_conteudo_central_apc">
+      <!-- MENU LATERAL -->
+      <div class="container_menu_l_apc float_left borda_preta_1 margem_l_20">
+        <div class="container_img_menu_apc centro_lr borda_preta_1 margem_t_20">
+          <div class="item_img_menu_l_apc ">
+
+          </div>
+        </div>
+        <!-- NOME USUÁRIO -->
+        <div class="container_nome_usuario_apc margem_t_10">
+          <div class="item_nome_usuario_apc centro_lr align_center preenche_t_15 fs_18 negrito txt_branco">
+            Nome de Usuário
+          </div>
+        </div>
+      </div>
+      <!-- PARTE AGENDA -->
+      <div class="container_principal_agenda_apc centro_lr float_left margem_l_20">
+        <!-- SEGURA DATAS -->
+        <div class="container_datas_apc float_left preenche_l_10">
+
+          <div class="item_datas_agenda_apc preenche_t_30 margem_t_20 align_center fs_20 negrito">
+            Data
+          </div>
+
+          <div class="item_datas_agenda_apc preenche_t_30 margem_t_30 align_center fs_20 negrito">
+            Data
+          </div>
+
+          <div class="item_datas_agenda_apc preenche_t_30 margem_t_30 align_center fs_20 negrito">
+            Data
+          </div>
+
+          <div class="item_datas_agenda_apc preenche_t_30 margem_t_30 align_center fs_20 negrito">
+            Data
+          </div>
+
+          <div class="item_datas_agenda_apc preenche_t_30 margem_t_30 align_center fs_20 negrito">
+            Data
+          </div>
+
+          <div class="item_datas_agenda_apc preenche_t_30 margem_t_30 align_center fs_20 negrito">
+            Data
+          </div>
+
+          <div class="item_datas_agenda_apc preenche_t_30 margem_t_30 align_center fs_20 negrito">
+            Data
+          </div>
+        </div>
+
+        <!-- SEGURA CAIXAS -->
+
+        <div class="container_caixas_agenda_apc float_left margem_l_100">
+
+          <div class="item_caixas_agenda_apc centro_lr borda_preta_1 preenche_t_30 margem_t_20 align_center fs_20 negrito">
+
+          </div>
+
+          <div class="item_caixas_agenda_apc centro_lr borda_preta_1 preenche_t_30 margem_t_30 align_center fs_20 negrito">
+
+          </div>
+
+          <div class="item_caixas_agenda_apc centro_lr borda_preta_1 preenche_t_30 margem_t_30 align_center fs_20 negrito">
+
+          </div>
+
+          <div class="item_caixas_agenda_apc centro_lr borda_preta_1 preenche_t_30 margem_t_30 align_center fs_20 negrito">
+
+          </div>
+
+          <div class="item_caixas_agenda_apc centro_lr borda_preta_1 preenche_t_30 margem_t_30 align_center fs_20 negrito">
+
+          </div>
+
+          <div class="item_caixas_agenda_apc centro_lr borda_preta_1 preenche_t_30 margem_t_30 align_center fs_20 negrito">
+
+          </div>
+
+          <div class="item_caixas_agenda_apc centro_lr borda_preta_1 preenche_t_30 margem_t_30 align_center fs_20 negrito">
+
+          </div>
+        </div>
+
+        <!-- PROPAGANDA -->
+
+        <div class="container_propaganda_apc borda_preta_1 float_left margem_l_70 margem_t_20 bg_branco">
+          <div class="item_propaganda_apc">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
