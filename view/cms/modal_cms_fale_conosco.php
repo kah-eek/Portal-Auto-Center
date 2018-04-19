d9a484ed66fed49d6a69b95c9e9b9dd08f0eddb8
IgorRios27 <riosigor27@gmail.com>
2018-04-19 16:33:58 -0300

    fale conosco do cms em andamento

---
+++ view/cms/modal_cms_fale_conosco.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="../css/cms/cms_modal_fale_conosco.css">
+    <link rel="stylesheet" href="../css/padroes.css">
+  </head>
+  <div class="container_principal_fl">
+    dddddddddddddddddddddddddddddddd
+  </div>
+</html>
