2b8e6dafd71f5323fce2c6c194c02a61d3ae9ef3
IgorRios27 <riosigor27@gmail.com>
2018-04-19 19:39:07 -0300

    tela cms produtos cadastrados

---
+++ view/cms/modal_cms_produtos_home.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="../css/cms/cms_modal_produtos_home.css">
+    <link rel="stylesheet" href="../css/padroes.css">
+  </head>
+  <div class="container_principal_ph float_left">
+    <div class="container_titulo_ph margem_t_50 borda_preta_1 bg_verde_vivo sombra_preta_20 centro_lr">
+      <div class="item_titulo_ph align_center preenche_t_20 fs_25 negrito">
+        Produtos Cadastrados
+      </div>
+    </div>
+
+    <!-- PARTE PRODUTO -->
+    <div class="container_produto_ph centro_lr margem_t_10">
+      <div class="item_produto_ph float_left">
+        <!-- IMAGEM -->
+        <div class="container_img_produto_ph float_left centro_lr">
+          <div class="item_img_produto_ph centro_lr margem_t_20 bg_branco">
+
+          </div>
+        </div>
+        <!-- NOME E VALOR DO PRODUTO -->
+        <div class="container_caixa_desc_ph float_left centro_lr">
+          <div class="item_caixa_desc_ph preenche_t_10 fs_25 negrito align_center borda_preta_1 bg_verde_vivo sombra_preta_20 margem_t_10 centro_lr">
+            Nome do Produto
+          </div>
+        </div>
+
+        <div class="container_caixa_desc_ph float_left centro_lr">
+          <div class="item_caixa_desc_ph preenche_t_10 fs_25 negrito align_center borda_preta_1 bg_verde_vivo sombra_preta_20 margem_t_20 centro_lr">
+            Valor do Produto
+          </div>
+        </div>
+      </div>
+      <!-- PARTE CHECKBOX -->
+      <div class="container_ckb_ph float_left margem_t_180 bg_verde_vivo">
+
+        <div class="item_ckb_ph float_left bg_verde_vivo margem_l_15 margem_t_10">
+          <input class="item_ckb" type="checkbox" name="ckb_ph" value="">
+        </div>
+
+        <div class="txt_ativo float_left">
+          <div class="item_txt_ativo preenche_t_10 fs_25 bg_verde_vivo align_center">
+            Ativo
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</html>
