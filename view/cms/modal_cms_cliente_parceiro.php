f91e4c820ac48548aa3c494099e6bd126a6a10e5
IgorRios27 <riosigor27@gmail.com>
2018-04-19 16:30:29 -0300

    tela visualizacao de cliente/parceiro cadastrados pronta

---
+++ view/cms/modal_cms_cliente_parceiro.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="../css/cms/cms_modal_cliente_parceiro.css">
+    <link rel="stylesheet" href="../css/padroes.css">
+  </head>
+  <div class="container_principal_cp_cadastrados">
+    <!-- TITULO CLIENTE -->
+    <div class="container_titulo_cp float_left margem_l_80 margem_t_50 borda_preta_1 bg_verde_vivo sombra_preta_20">
+      <div class="item_titulo_cp preenche_t_20 margem_l_30 fs_25 negrito">
+        Clientes
+      </div>
+    </div>
+    <!-- VISUALIZAÇÃO DE CLIENTE -->
+    <div class=" float_left container_visualiza_cp margem_t_10 margem_l_80">
+      <div class="container_topicos">
+        <div class="item_titulo_topicos float_left preenche_t_10 fs_25 negrito margem_l_10 align_center borda_preta_1 bg_verde_vivo sombra_preta_20">
+          Nome
+        </div>
+
+        <div class="item_titulo_topicos float_left preenche_t_10 fs_25 negrito margem_l_10 align_center borda_preta_1 bg_verde_vivo sombra_preta_20">
+          Telefone
+        </div>
+
+        <div class="item_titulo_topicos float_left preenche_t_10 fs_25 negrito margem_l_10 align_center borda_preta_1 bg_verde_vivo sombra_preta_20">
+          E-mail
+        </div>
+
+        <div class="ativo_ver float_left preenche_t_10 fs_25 negrito margem_l_10 align_center borda_preta_1 bg_verde_vivo sombra_preta_20">
+          Ativo
+        </div>
+
+        <div class="ativo_ver float_left preenche_t_10 fs_25 negrito margem_l_10 align_center borda_preta_1 bg_verde_vivo sombra_preta_20">
+          Ver
+        </div>
+      </div>
+
+      <!-- DADOS QUE VIRÃO DO BANCO -->
+      <div class="container_dados_topicos margem_t_5">
+        <div class="item_dados_topicos float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          Igor
+        </div>
+
+        <div class="item_dados_topicos float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          (11) 99999-9999
+        </div>
+
+        <div class="item_dados_topicos float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          igor-lalala@outlook.com
+        </div>
+
+        <div class="ativo_ver_dados float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          <input class="tamanho_ckb" type="checkbox" name="ckb_ativo">
+        </div>
+
+        <div class="ativo_ver_dados float_left preenche_t_10 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          <i class="material-icons" style="font-size:30px;">visibility</i>
+        </div>
+      </div>
+    </div>
+
+    <!-- PARTE PARCEIRO -->
+
+    <div class="container_titulo_cp float_left margem_l_80 margem_t_50 borda_preta_1 bg_verde_vivo sombra_preta_20">
+      <div class="item_titulo_cp preenche_t_20 margem_l_30 fs_25 negrito">
+        Parceiros
+      </div>
+    </div>
+    <!-- VISUALIZAÇÃO DE PARCEIRO -->
+    <div class=" float_left container_visualiza_cp margem_t_10 margem_l_80">
+      <div class="container_topicos">
+        <div class="item_titulo_topicos float_left preenche_t_10 fs_25 negrito margem_l_10 align_center borda_preta_1 bg_verde_vivo sombra_preta_20">
+          Nome
+        </div>
+
+        <div class="item_titulo_topicos float_left preenche_t_10 fs_25 negrito margem_l_10 align_center borda_preta_1 bg_verde_vivo sombra_preta_20">
+          CNPJ
+        </div>
+
+        <div class="item_titulo_topicos float_left preenche_t_10 fs_25 negrito margem_l_10 align_center borda_preta_1 bg_verde_vivo sombra_preta_20">
+          E-mail
+        </div>
+
+        <div class="ativo_ver float_left preenche_t_10 fs_25 negrito margem_l_10 align_center borda_preta_1 bg_verde_vivo">
+          Ativo
+        </div>
+
+        <div class="ativo_ver float_left preenche_t_10 fs_25 negrito margem_l_10 align_center borda_preta_1 bg_verde_vivo sombra_preta_20">
+          Ver
+        </div>
+      </div>
+
+      <!-- DADOS QUE VIRÃO DO BANCO -->
+      <div class="container_dados_topicos margem_t_5">
+        <div class="item_dados_topicos float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          Igor
+        </div>
+
+        <div class="item_dados_topicos float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          (11) 99999-9999
+        </div>
+
+        <div class="item_dados_topicos float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          igor-lalala@outlook.com
+        </div>
+
+        <div class="ativo_ver_dados float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          <input class="tamanho_ckb" type="checkbox" name="ckb_ativo">
+        </div>
+
+        <div class="ativo_ver_dados float_left preenche_t_10 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          <i class="material-icons" style="font-size:30px;">visibility</i>
+        </div>
+      </div>
+    </div>
+  </div>
+</html>
