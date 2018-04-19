3feecf9179f2eb2e66745413d53de9d3b14487ec
IgorRios27 <riosigor27@gmail.com>
2018-04-19 17:20:27 -0300

    tela fale conosco do cms pronta

--- view/cms/modal_cms_fale_conosco.php
+++ view/cms/modal_cms_fale_conosco.php
@@ -6,7 +6,174 @@
     <link rel="stylesheet" href="../css/cms/cms_modal_fale_conosco.css">
     <link rel="stylesheet" href="../css/padroes.css">
   </head>
-  <div class="container_principal_fl">
-    dddddddddddddddddddddddddddddddd
+  <div class="container_principal_fc float_left">
+    <div class="container_titulo_fc margem_t_50 borda_preta_1 bg_verde_vivo sombra_preta_20 centro_lr">
+      <div class="item_titulo_fc align_center preenche_t_20 fs_25 negrito">
+        Fale Conosco
+      </div>
+    </div>
+    <!-- VISUALIZAÇÃO DE CLIENTE -->
+    <div class="container_visualiza_fc centro_lr margem_t_10">
+      <div class="container_topicos_fc centro_lr">
+        <div class="item_titulo_topicos_fc float_left preenche_t_10 fs_25 negrito margem_l_10 align_center borda_preta_1 bg_verde_vivo sombra_preta_20">
+          Nome
+        </div>
+
+        <div class="item_titulo_topicos_fc float_left preenche_t_10 fs_25 negrito margem_l_10 align_center borda_preta_1 bg_verde_vivo sombra_preta_20">
+          E-mail
+        </div>
+
+        <div class="item_titulo_topicos_fc float_left preenche_t_10 fs_25 negrito margem_l_10 align_center borda_preta_1 bg_verde_vivo sombra_preta_20">
+          Mensagem
+        </div>
+
+        <div class="ativo_ver_fc float_left preenche_t_10 fs_25 negrito margem_l_10 align_center borda_preta_1 bg_verde_vivo sombra_preta_20">
+          Delete
+        </div>
+
+        <div class="ativo_ver_fc float_left preenche_t_10 fs_25 negrito margem_l_10 align_center borda_preta_1 bg_verde_vivo sombra_preta_20">
+          Ver
+        </div>
+      </div>
+
+      <!-- DADOS QUE VIRÃO DO BANCO -->
+      <div class="container_dados_topicos_fc margem_t_5">
+        <div class="item_dados_topicos_fc float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          Igor
+        </div>
+
+        <div class="item_dados_topicos_fc float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          igor-lalala@outlook.com
+        </div>
+
+        <div class="item_dados_topicos_fc float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20 justificado">
+          No menu Inserir, as galerias incluem itens que são projetados para corresponder à aparência geral do documento.
+        </div>
+
+        <div class="ativo_ver_dados_fc float_left preenche_t_10 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          <i class="material-icons" style="font-size:30px;">delete_forever</i>
+        </div>
+
+        <div class="ativo_ver_dados_fc float_left preenche_t_10 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          <i class="material-icons" style="font-size:30px;">visibility</i>
+        </div>
+      </div>
+
+      <!-- DADOS QUE VIRÃO DO BANCO -->
+      <div class="container_dados_topicos_fc margem_t_5">
+        <div class="item_dados_topicos_fc float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          Igor
+        </div>
+
+        <div class="item_dados_topicos_fc float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          igor-lalala@outlook.com
+        </div>
+
+        <div class="item_dados_topicos_fc float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20 justificado">
+          No menu Inserir, as galerias incluem itens que são projetados para corresponder à aparência geral do documento.
+        </div>
+
+        <div class="ativo_ver_dados_fc float_left preenche_t_10 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          <i class="material-icons" style="font-size:30px;">delete_forever</i>
+        </div>
+
+        <div class="ativo_ver_dados_fc float_left preenche_t_10 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          <i class="material-icons" style="font-size:30px;">visibility</i>
+        </div>
+      </div>
+
+      <!-- DADOS QUE VIRÃO DO BANCO -->
+      <div class="container_dados_topicos_fc margem_t_5">
+        <div class="item_dados_topicos_fc float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          Igor
+        </div>
+
+        <div class="item_dados_topicos_fc float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          igor-lalala@outlook.com
+        </div>
+
+        <div class="item_dados_topicos_fc float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20 justificado">
+          No menu Inserir, as galerias incluem itens que são projetados para corresponder à aparência geral do documento.
+        </div>
+
+        <div class="ativo_ver_dados_fc float_left preenche_t_10 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          <i class="material-icons" style="font-size:30px;">delete_forever</i>
+        </div>
+
+        <div class="ativo_ver_dados_fc float_left preenche_t_10 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          <i class="material-icons" style="font-size:30px;">visibility</i>
+        </div>
+      </div>
+
+      <!-- DADOS QUE VIRÃO DO BANCO -->
+      <div class="container_dados_topicos_fc margem_t_5">
+        <div class="item_dados_topicos_fc float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          Igor
+        </div>
+
+        <div class="item_dados_topicos_fc float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          igor-lalala@outlook.com
+        </div>
+
+        <div class="item_dados_topicos_fc float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20 justificado">
+          No menu Inserir, as galerias incluem itens que são projetados para corresponder à aparência geral do documento.
+        </div>
+
+        <div class="ativo_ver_dados_fc float_left preenche_t_10 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          <i class="material-icons" style="font-size:30px;">delete_forever</i>
+        </div>
+
+        <div class="ativo_ver_dados_fc float_left preenche_t_10 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          <i class="material-icons" style="font-size:30px;">visibility</i>
+        </div>
+      </div>
+
+      <!-- DADOS QUE VIRÃO DO BANCO -->
+      <div class="container_dados_topicos_fc margem_t_5">
+        <div class="item_dados_topicos_fc float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          Igor
+        </div>
+
+        <div class="item_dados_topicos_fc float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          igor-lalala@outlook.com
+        </div>
+
+        <div class="item_dados_topicos_fc float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20 justificado">
+          No menu Inserir, as galerias incluem itens que são projetados para corresponder à aparência geral do documento.
+        </div>
+
+        <div class="ativo_ver_dados_fc float_left preenche_t_10 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          <i class="material-icons" style="font-size:30px;">delete_forever</i>
+        </div>
+
+        <div class="ativo_ver_dados_fc float_left preenche_t_10 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          <i class="material-icons" style="font-size:30px;">visibility</i>
+        </div>
+      </div>
+
+      <!-- DADOS QUE VIRÃO DO BANCO -->
+      <div class="container_dados_topicos_fc margem_t_5">
+        <div class="item_dados_topicos_fc float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          Igor
+        </div>
+
+        <div class="item_dados_topicos_fc float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          igor-lalala@outlook.com
+        </div>
+
+        <div class="item_dados_topicos_fc float_left preenche_t_15 margem_l_10 align_center borda_preta_1 sombra_preta_20 justificado">
+          No menu Inserir, as galerias incluem itens que são projetados para corresponder à aparência geral do documento.
+        </div>
+
+        <div class="ativo_ver_dados_fc float_left preenche_t_10 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          <i class="material-icons" style="font-size:30px;">delete_forever</i>
+        </div>
+
+        <div class="ativo_ver_dados_fc float_left preenche_t_10 margem_l_10 align_center borda_preta_1 sombra_preta_20">
+          <i class="material-icons" style="font-size:30px;">visibility</i>
+        </div>
+      </div>
+    </div>
+
   </div>
 </html>
