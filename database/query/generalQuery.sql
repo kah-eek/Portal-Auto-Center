9be4cd3fba5520ee3eb285855c62da0433e5d2cd
Caique <caique_m_oliveira@outlook.com>
2018-05-01 12:43:20 -0300

    adiciona o campi ativo na tbl_imagem_produto_parceiro

--- database/query/generalQuery.sql
+++ database/query/generalQuery.sql
@@ -1,13 +1,9 @@
+SELECT * FROM tbl_produto;
+SELECT * FROM tbl_categoria_produto;
+
+SELECT * FROM tbl_imagem_produto_parceiro;
 SELECT * FROM tbl_imagem_veiculo_parceiro;
 
 SELECT * FROM view_imagem_veiculo_parceiro;
 
-SELECT * FROM tbl_veiculo_parceiro;
-SELECT * FROM tbl_veiculo;
-SELECT * FROM tbl_tipo_veiculo;
-
-'4', '1', '../pictures/galeria/car.jpg', '1'
-
-'3', '1', '../pictures/galeria/blue.jpg', '1'
-
-
+SELECT *  FROM tbl_imagem_produto_parceiro;
