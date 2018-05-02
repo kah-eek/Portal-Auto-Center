d5618dc1fcafed9aac2a535bceeeaa8bf82509d0
HENRIQUE OTREMBA DOS SANTOS <16254852@CFP127ALUNO.LOCAL>
2018-05-02 11:23:55 -0300

    Fazendo a tela cliente parceiro

---
+++ model/ClienteParceiroDAO.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+* @author Henrique Otremba dos Santos
+* @date 22/04/2018
+*/
+
+// Imports
+require_once('controller/MySql_class.php');
+
+class ClienteParceiroDAO
+{
+
+  /**
+  * Atualiza a tabela responsável pelos registros da tela sobre a empresa
+  * @param $sobreEmpresaObj Objeto SobreEmpresa qual será atualizado no banco de dados
+  * @return true SobreEmpresa atualizado com sucesso na base de dados
+  * @return false Falha ao tentar atualizar o SobreEmpresa na base de dados
+  */
+  function atualizar($clienteParceiroObj)
+  {
+    // Instância de acesso ao db
+    $mySql = new MySql();
+
+    // Abre uma nova conexão com o db
+    $con = $mySql->getConnection();
+
+    $stmt = $con->prepare(
+      'UPDATE tbl_sobre_cliente_parceiro SET '.
+        'imagem = ?,'.
+        'descricao = ? '.
+        'WHERE '.
+        'id_sobre_cliente_parceiro = ?'
+    );
+
+    $stmt->bindParam(1, $sobreEmpresaObj->imagem);
+    $stmt->bindParam(2, $sobreEmpresaObj->descricao);
+    $stmt->bindParam(3, $sobreEmpresaObj->idSobreClienteParceiro);
+
+    // Verifica se a atualização do registro ocorreu com sucesso e retorna a resposta adquirida
+    $result = $stmt->execute() ? true : false;
+
+    // Fecha a conexão com o db
+    $con = null;
+
+    return $result;
+
+  }
+
+}
+
+?>
