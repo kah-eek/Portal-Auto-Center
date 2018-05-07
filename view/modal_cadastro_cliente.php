1b9eff248ae658cb65a4dc450212e2f79808aaa6
IGOR BEZERRA RIOS <16254853@CFP127ALUNO.LOCAL>
2018-05-07 11:02:04 -0300

    cadastro de cliente quase concluido

--- view/modal_cadastro_cliente.php
+++ view/modal_cadastro_cliente.php
@@ -1,3 +1,87 @@
+<?php
+
+$id_cliente="";
+$id_endereco="";
+$id_estado="";
+$id_nivel_usuario="";
+
+$nome="";
+$DtNasc="";
+$cpf="";
+$email="";
+$celular="";
+$telefone="";
+$sexo="";
+// ENDEREÇO
+$logradouro="";
+$numero="";
+$cidade="";
+$cep="";
+$bairro="";
+$complemento="";
+// LOGIN
+$id_usuario="";
+$usuario="";
+$senha="";
+
+$botao="Salvar";
+
+$conexao=mysql_connect('localhost', 'root', 'bcd127');
+
+    //ATIVA O DATABASE A SER UTILIZADO NO PROJETO
+    mysql_select_db('db_auto_center');
+
+  if(isset($_POST["btnSalvar"]))
+  {
+    //RESGATA OS DADOS FORNECIDOS PELO CLIENTE.
+    //USANDO O MÉTODO POST, CONFORME ESCOLHIDO NO FORM.
+
+    // DADOS DE PESSOA
+    // 1;
+    $nome=$_POST["txtNome"];
+    $DtNasc=$_POST["dtNasc"];
+    $cpf=$_POST["txtCpf"];
+    $email=$_POST["txtEmail"];
+    $celular=$_POST["txtCelular"];
+    $telefone=$_POST["txtTelefone"];
+    $sexo=$_POST["sexo"];
+
+    // ENDEREÇO
+    $id_endereco=$_POST["id_endereco"];
+    $logradouro=$_POST["txtLogradouro"];
+    $numero=$_POST["txtNumero"];
+    $cidade=$_POST["txtCidade"];
+    $id_estado=$_POST["slcEstado"];
+    $cep=$_POST["txtCep"];
+    $bairro=$_POST["txtBairro"];
+    $complemento=$_POST["txtComplemento"];
+
+    // LOGIN
+    $usuario=$_POST["txtUsuario"];
+    $senha=$_POST["txtSenha"];
+    $id_nivel_usuario=$_POST["slcNivel"];
+
+    if($_POST["btnsalvar"]=='Salvar')
+      {
+        //MONTA O SCRIPT PARA ENVIAR PARA O BD
+      $sql = "INSERT INTO tbl_cliente(id_endereco,id_estado,id_nivel_usuario,nome,DtNasc,cpf,email,celular,telefone,sexo,logradouro,numero,cidade,cep,bairro,complemento,usuario,senha)
+          values('".$id_endereco."','".$id_estado."','".$id_nivel_usuario."','".$nome."','".$DtNasc."','".$cpf."','".$email."','".$celular."',
+          '".$telefone."','".$sexo."','".$logradouro."','".$numero."',
+          '".$cidade."','".$cep."','".$bairro."','".$complemento."','".$usuario."',
+          '".$senha."')";
+      }
+
+      //EXECUTA O SCRIPT NO BD
+      // mysql_query($sql);
+      //
+      //
+      // header('location:modal_cadastro_cliente.php');
+
+      // echo($sql);
+
+  }
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>
@@ -8,40 +92,40 @@
   <body>
     <div class="container_principal_mc_cp bg_branco">
       <!-- TITULO -->
-      <div class="container_titulo">
+      <!-- <div class="container_titulo">
         <div class="item_titulo float_left txt_preto titulo align_center">
           Cadastro Clientes
         </div>
         <div class="container_btn_sair float_right negrito">
           <a class="btn_x fs_20 txt_preto" href="cliente_parceiro.php?page=cliente_parceiro">X</a>
         </div>
-      </div>
+      </div> -->
       <!-- IMAGENS -->
-      <div class="container_imagens margem_t_10 centro_lr">
+      <!-- <div class="container_imagens margem_t_10 centro_lr"> -->
 
         <!-- LABEL IMAGEM CLIENTE -->
-        <label for="btn_img_cliente">
+        <!-- <label for="btn_img_cliente">
           <div class="item_imagens bg_branco float_left">
 
           </div>
         </label>
-        <input type="file" class="display_none" id="btn_img_cliente" name="btn_img_cliente" value="">
+        <input type="file" class="display_none" id="btn_img_cliente" name="btn_img_cliente" value=""> -->
 
         <!-- LABEL IMAGEM VEICULO -->
-        <label for="btn_img_veiculo">
+        <!-- <label for="btn_img_veiculo">
           <div class="item_imagens bg_branco float_right">
 
           </div>
           <input class="display_none" id="btn_img_veiculo" type="file" name="btn_img_veiculo" value="">
         </label>
-      </div>
+      </div> -->
       <!-- FORM -->
-      <form class="form_cadastro_cliente margem_t_5 float_left" action="modal_cadastro_cliente.php" method="post">
+      <form class="form_cadastro_cliente margem_t_5 float_left" action="modal_cadastro_cliente.php" method="POST">
         <div class="container_infs">
 
           <!-- INPUT NOME -->
           <div class="segura_input">
-            <input class="input_text txt_preto margem_t_5" placeholder="Nome" type="text" name="txt_nome" value="">
+            <input class="input_text txt_preto margem_t_5" placeholder="Nome" type="text" name="txtNome" value="">
           </div>
 
           <!-- TITULO DATA -->
@@ -53,27 +137,27 @@
 
           <!-- INPUT CALENDÁRIO -->
           <div class="segura_input float_left margem_t_5">
-            <input class="data_nasc margem_10" type="date" name="dt_nasc" value="">
+            <input class="data_nasc margem_10" type="date" name="dtNasc" value="">
           </div>
 
           <!-- INPUT CPF -->
           <div class="segura_input float_left margem_t_5">
-            <input class="input_text txt_preto margem_t_10" placeholder="Cpf" type="text" name="txt_cpf" value="">
+            <input class="input_text txt_preto margem_t_10" placeholder="Cpf" type="text" name="txtCpf" value="">
           </div>
 
           <!-- INPUT EMAIL -->
           <div class="segura_input float_left margem_t_5">
-            <input class="input_text txt_preto margem_t_10" placeholder="Email Address" type="text" name="txt_email" value="">
+            <input class="input_text txt_preto margem_t_10" placeholder="Email Address" type="text" name="txtEmail" value="">
           </div>
 
           <!-- INPUT CELULAR -->
           <div class="segura_input float_left margem_t_5">
-            <input class="input_text txt_preto margem_t_10" placeholder="Celular" type="text" name="txt_celular" value="">
+            <input class="input_text txt_preto margem_t_10" placeholder="Celular" type="text" name="txtCelular" value="">
           </div>
 
           <!-- INPUT TELEFONE -->
           <div class="segura_input float_left margem_t_5">
-            <input class="input_text txt_preto margem_t_10" placeholder="Telefone" type="text" name="txt_telefone" value="">
+            <input class="input_text txt_preto margem_t_10" placeholder="Telefone" type="text" name="txtTelefone" value="">
           </div>
 
           <!-- INPUT RADIO -->
@@ -94,32 +178,82 @@
 
           <!-- INPUT RUA -->
           <div class="segura_input float_left">
-            <input class="input_text txt_preto margem_t_5" placeholder="Rua" type="text" name="txt_rua" value="">
+            <input class="input_text txt_preto margem_t_5" placeholder="Rua" type="text" name="txtRua" value="">
           </div>
 
           <!-- INPUT NUMERO -->
           <div class="segura_input float_left margem_t_5">
-            <input class="input_text txt_preto margem_t_10" placeholder="Número" type="text" name="txt_numero" value="">
+            <input class="input_text txt_preto margem_t_10" placeholder="Número" type="text" name="txtNumero" value="">
           </div>
 
           <!-- INPUT CIDADE -->
           <div class="segura_input float_left margem_t_5">
-            <input class="input_text txt_preto margem_t_10" placeholder="Cidade" type="text" name="txt_cidade" value="">
+            <input class="input_text txt_preto margem_t_10" placeholder="Cidade" type="text" name="txtNidade" value="">
           </div>
+
+          <!-- SELECT ESTADO -->
+          <select class="select_opt margem_t_10 margem_l_30" name="slcEstado">
+            <?php
+              $sql="select * from tbl_estado order by id_estado desc";
+              $select=mysql_query($sql);
+              while($rsConsulta= mysql_fetch_array($select)){
+            ?>
+
+              <option class="select" value="slcEstado"><?php echo($rsConsulta['estado']) ?></option>>
+
+             <?php
+                }
+              ?>
+          </select>
 
           <!-- INPUT CEP -->
           <div class="segura_input float_left margem_t_5">
-            <input class="input_text txt_preto margem_t_10" placeholder="Cep" type="text" name="txt_cep" value="">
+            <input class="input_text txt_preto margem_t_10" placeholder="Cep" type="text" name="txtCep" value="">
           </div>
 
           <!-- INPUT BAIRRO -->
           <div class="segura_input float_left margem_t_5">
-            <input class="input_text txt_preto margem_t_10" placeholder="Bairro" type="text" name="txt_bairro" value="">
+            <input class="input_text txt_preto margem_t_10" placeholder="Bairro" type="text" name="txtBairro" value="">
           </div>
 
           <!-- INPUT COMPLEMENTO -->
           <div class="segura_input float_left margem_t_5">
-            <input class="input_text txt_preto margem_t_10" placeholder="Complemento" type="text" name="txt_complemento" value="">
+            <input class="input_text txt_preto margem_t_10" placeholder="Complemento" type="text" name="txtComplemento" value="">
+          </div>
+
+          <!-- TITULO LOGIN -->
+          <div class="titulo_form">
+            <div class="item_titulo_form titulo margem_t_20 float_left">
+              Login
+            </div>
+          </div>
+
+          <!-- INPUT USER -->
+          <div class="segura_input float_left margem_t_10">
+            <input class="input_text txt_preto" placeholder="Usuário" type="text" name="txtUsuario" value="">
+          </div>
+
+          <!-- INPUT SENHA -->
+          <div class="segura_input float_left margem_t_10">
+            <input class="input_text txt_preto" placeholder="Senha" type="password" name="txtSenha" value="">
+          </div>
+
+          <select class="select_opt margem_l_30" name="slcNivel">
+            <?php
+              $sql="select * from tbl_nivel_usuario order by id_nivel_usuario desc";
+              $select=mysql_query($sql);
+              while($rsConsulta= mysql_fetch_array($select)){
+            ?>
+
+              <option class="select" name="slcNivel" value=""><?php echo($rsConsulta['nivel']) ?></option>>
+
+             <?php
+                }
+              ?>
+          </select>
+
+          <div class="segura_bt float_left">
+            <input href="" class="input_submit_cc margem_t_25 sombra_preta_b_15" type="submit" name="btnSalvar" value="Salvar">
           </div>
         </div>
       </form>
@@ -168,27 +302,8 @@
           <div class="divisor_c margem_t_30 float_left">
 
           </div>
-          <!-- TITULO LOGIN -->
-          <div class="titulo_form">
-            <div class="item_titulo_form titulo margem_t_20 float_left">
-              Login
-            </div>
-          </div>
-
-          <!-- INPUT USER -->
-          <div class="segura_input float_left margem_t_10">
-            <input class="input_text txt_preto" placeholder="Usuário" type="text" name="txt_use" value="">
-          </div>
-
-          <!-- INPUT SENHA -->
-          <div class="segura_input float_left margem_t_10">
-            <input class="input_text txt_preto" placeholder="Senha" type="password" name="txt_senha" value="">
-          </div>
         </div>
       </form>
-      <div class="segura_bt float_left">
-        <input class="input_submit margem_t_25 sombra_preta_b_15" type="submit" name="btn_salvar" value="Salvar">
-      </div>
     </div>
   </body>
 </html>
