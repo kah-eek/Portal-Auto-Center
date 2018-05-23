5282f8af7c1bdf2b6534a3ad99f06e7dd98fefc7
HENRIQUE OTREMBA DOS SANTOS <16254852@CFP127ALUNO.LOCAL>
2018-05-23 11:14:46 -0300

    Rede social 80%

--- view/modal_rede_social.php
+++ view/modal_rede_social.php
@@ -1,7 +1,6 @@
 <?php
 require_once("../database/conect.php");
 Conexao_db();
-
  ?>
 
 
@@ -18,36 +17,55 @@ Conexao_db();
       <div class="container_menu">
         <div class="container_segura_menu">
           <div class="container_logo float_left">
-
+            <img src="pictures/logo/portal_auto_center.png" width="80px" alt="">
+            INICIO
           </div>
           <div class="container_agenda float_left">
             <i class = "material-icons fs_60">
               today
             </i>
+            AGENDA
           </div>
           <div class="container_perfil float_left">
-
+            <i class="material-icons fs_60">
+              person
+            </i>
+            PERFIL
           </div>
           <div class="container_sair float_left fs_40">
-            sair
+            SAIR
           </div>
         </div>
       </div>
     </header>
-    <article>
+
+      <?php
+      $sql1 = "SELECT tbl_post_rede_social.*,
+        tbl_usuario.usuario FROM db_auto_center.tbl_post_rede_social
+        INNER JOIN tbl_usuario
+        ON tbl_usuario.id_usuario = tbl_post_rede_social.id_usuario;
+        ";
+        $select1 = mysql_query($sql1);
+        while ($rsFC1 = mysql_fetch_array($select1))
+        {
+
+      ?>
+      <article class="article sombra_preta_30 margem_t_20" >
       <div class="container_usuario">
         <div class="container_segura_usuario">
           <div class="container_foto_usuario float_left">
 
           </div>
-          <div class="container_nome_usuario float_left">
-
+          <div class="container_nome_usuario float_left fs_30 titulo">
+            <?php echo($rsFC1['usuario']) ?>
           </div>
         </div>
       </div>
-      <div class="container_foto_postada">
 
+      <div class="container_foto_postada">
+        <img src="<?php echo($rsFC1['foto']) ?>" alt="">
       </div>
+
       <div class="container_curtir_comentar">
         <div class="curtir">
           <i class="material-icons fs_50 float_left">
@@ -60,19 +78,37 @@ Conexao_db();
           </i>
         </div>
       </div>
-      <div class="container_comentarios">
+      <?php
+      $sql = "SELECT tbl_comentario_post.*,
+      tbl_usuario.usuario FROM db_auto_center.tbl_comentario_post
+      INNER JOIN tbl_usuario
+      ON tbl_usuario.id_usuario = tbl_comentario_post.id_usuario;";
+        $select = mysql_query($sql);
+        while ($rsFC = mysql_fetch_array($select))
+        {
+
+      ?>
+      <div class="container_comentarios bsuavizada_40 sombra_preta_10">
         <div class="usuario_comentario">
-          <div class="foto_usuario_comentario">
+          <div class="foto_usuario_comentario float_left margem_l_15 margem_t_5">
 
           </div>
-          <div class="nome_usuario_comentario">
-
+          <div class="nome_usuario_comentario titulo border">
+            <?php echo($rsFC['usuario']) ?>
           </div>
         </div>
-        <div class="comentario">
-          
+        <div class="comentario conteudo justificado preenche_l_20 espacamento_linha_25 txt_preto bsuavizada_40">
+          <?php echo($rsFC['comentario']) ?>
         </div>
       </div>
-    </article>
+      <?php
+        }
+        ?>
+      </article>
+      <?php
+
+      }
+      ?>
+
   </body>
 </html>
