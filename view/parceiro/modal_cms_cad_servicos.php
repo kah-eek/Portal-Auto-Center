a970e04582924712aa86229aa7d71fb27d2652b8
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2018-06-03 20:36:38 -0300

    finaliza o layout da tela modal_cms_cad_servicos

--- view/parceiro/modal_cms_cad_servicos.php
+++ view/parceiro/modal_cms_cad_servicos.php
@@ -36,51 +36,65 @@ if(isset($_POST["btnSalvar"]))
   <head>
     <meta charset="utf-8">
     <title>Cadastro de produtos</title>
-    <link rel="stylesheet" href="../css/parceiro/modal_cms_cad_servicos.css">
+    <link rel="stylesheet" href="../css/normalize.css">
     <link rel="stylesheet" href="../css/padroes.css">
+    <link rel="stylesheet" href="../css/parceiro/modal_cms_cad_servicos.css">
   </head>
-    <body>
-      <!-- <div class="container_campo float_left fs_30">
-        <div class="campo">
-          Nome
+  <body class="body">
+
+      <header class="header">
+        <img src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3159b23f37c4f3954e97072e00e975ab&dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb">
+
+        <h1 class="page-title">Auto Fast</h1>
+
+        <div class="saudacao">
+          <p>Bem-vindo</p>
+          <p>Caique M. Oliveira</p>
         </div>
-        <div class="campo">
-          Preco
-        </div>
-        <div class="campo">
-          Garantia
-        </div>
-        <div class="campo">
-          Descrição
-        </div>
-      </div> -->
-      <form name="frmCadastroServico" method="POST" action="">
-        <div class="container_input float_left margem_l_5">
-          <div class="input ">
-            <input class="input_text_111" type="text" name="txt_nome" placeholder="Nome do Serviço">
-          </div>
-          <div class="input ">
-            <input class="input_text_111" type="text" name="txt_preco" placeholder="Preço do Serviço">
-          </div>
-          <div class="input ">
-            <select class="input_select" name="slcGarantia">
-              <option value="">SELECIONE UMA GARANTIA</option>
+        <a class="return-button" href="cms_adm_parceiro.php">
+          <i class="material-icons">
+            keyboard_arrow_left
+          </i>
+        </a>
+      </header>
+
+      <div class="blank-space"></div>
+
+      <div class="main">
+        <form name="frmCadastroServico" method="POST" action="">
+          
+          <div class="cont-princ">
+
+            <label class="label-pr-h">Cadastro de Serviço</label>
+            <div class="divisor"></div>
+
+            <label for="txt_nome" class="field-label">Nome do Serviço</label>
+            <input id="txt_nome" class="android-input input-text" type="text" name="txt_nome">
+
+            <label for="txt_preco" class="field-label">Preço do Serviço</label>
+            <input id="txt_preco" class="android-input input-text" type="text" name="txt_preco">
+
+            <select class="select-pac" name="slcGarantia">
+              <option selected disabled value="">SELECIONE UMA GARANTIA</option>
               <option value="1 Mes">1 Meses</option>
               <option value="3 Meses">3 Meses</option>
               <option value="5 Meses">5 Meses</option>
               <option value="6 Meses">6 Meses</option>
               <option value="12 Meses">12 Meses</option>
             </select>
+
+            <label for="txt_desc" class="field-label">Descrição do Servicço</label>
+            <input id="txt_desc" class="android-input input-text" type="text" name="txt_desc">
+
           </div>
-          <div class="input">
-            <input class="input_text_111" type="text" name="txt_desc" placeholder="Descrição do Serviço">
-          </div>
-        </div>
-        <div class="input">
-          <div class="Container_btn">
-              <input class="input_btn" type="submit" name="btnSalvar" value="Salvar">
-          </div>
-        </div>
-      </form>
+
+          <input class="input-submit-cad-veic" type="submit" name="btnSalvar" value="Salvar Serviço">
+          
+        </form>
+      </div>
+
+      <script src="../js/jquery.js"></script>
+      <script src="../js/pac_framework.js"></script>
+
     </body>
 </html>
