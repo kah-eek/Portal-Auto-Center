5b4180c72420af968c5f9b36a7e3c25508220ae7
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2018-06-03 21:22:49 -0300

    finaliza o layout da tela editar_produto_home

--- view/parceiro/editar_produto_home.php
+++ view/parceiro/editar_produto_home.php
@@ -37,73 +37,172 @@ Conexao_db();
       <head>
         <meta charset="utf-8">
         <title>Editar Produtos</title>
+        <link rel="stylesheet" href="../css/normalize.css">
+        <link rel="stylesheet" href="../css/padroes.css">
         <link rel="stylesheet" href="../css/parceiro/editar_produto_home.css">
         <link rel="stylesheet" href="../css/parceiro/cms_agenda_parceiro.css">
-        <link rel="stylesheet" href="../css/padroes.css">
       </head>
-      <body>
-        <div class="Container_titulo">
-          <div class="titulo">
-            Modelo
-          </div>
-          <div class="titulo">
-            Cor
-          </div>
-          <div class="titulo">
-            Categoria
-          </div>
-          <div class="titulo">
-            Nome
-          </div>
-          <div class="titulo">
-            Preco
-          </div>
-          <div class="titulo">
-            Conteudo embalagem
-          </div>
-          <div class="titulo">
-            Garantia
-          </div>
-          <div class="titulo">
-            Descrição
-          </div>
-          <div class="titulo">
-            Observação
-          </div>
+      <body class="body">
+
+    <header class="header">
+      <img src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3159b23f37c4f3954e97072e00e975ab&dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb">
+
+      <h1 class="page-title">Auto Fast</h1>
+
+      <div class="saudacao">
+        <p>Bem-vindo</p>
+        <p>Caique M. Oliveira</p>
+      </div>
+      <a class="return-button" href="cms_adm_parceiro.php">
+        <i class="material-icons">
+          keyboard_arrow_left
+        </i>
+      </a>
+    </header>
+
+    <div class="blank-space"></div>
+
+    <div class="main">
+
+      <form name="frmCadastroVeiculo" method="POST" action="modal_cms_cadastro_veiculo.php">
+
+        <div class="form-container">
+          <label for="slcFabricante" class="titulo-cad-ve">Editar Produto</label>
+
+          <div class="divisor"></div>
+
+          <?php
+            if (isset($_GET['idFab']))
+              $IdFabricant = $_GET['idFab'];
+            else
+              $IdFabricant = 0;
+
+
+            ?>
+
+            <select class="select-pac" required name="slcFabricante">
+
+
+              <option selected disabled value="">Fabricante</option>
+              <?php
+              if ($IdFabricant<>0)
+              {
+                  $sql = "SELECT * FROM do frabricanteo where id_fabricante = ".$IdFabricant;
+                  $select = mysql_query($sql);
+                  while ($rsCV = mysql_fetch_array($select))
+                  {
+
+                    ?>
+                    <option value="<?php echo($rsCV['id_modelo_veiculo']) ?>"><?php echo($rsCV['modelo']) ?></option>
+                    <?php
+                  }
+                }
+                ?>
+            </select>
+
+
+
+            <select class="select-pac" required name="slcModelo">
+
+
+              <option selected disabled value="">Modelo</option>
+              <?php
+              if ($IdFabricant<>0)
+              {
+                  $sql = "SELECT * FROM tbl_modelo_veiculo where id_fabricante = ".$IdFabricant;
+                  $select = mysql_query($sql);
+                  while ($rsCV = mysql_fetch_array($select))
+                  {
+
+                    ?>
+                    <option value="<?php echo($rsCV['id_modelo_veiculo']) ?>"><?php echo($rsCV['modelo']) ?></option>
+                    <?php
+                  }
+                }
+                ?>
+            </select>
+
+             <select class="select-pac" required name="slcCor">
+
+
+              <option selected disabled value="">Cor do produto</option>
+              <?php
+              if ($IdFabricant<>0)
+              {
+                  $sql = "SELECT * FROM da cor = ".$IdFabricant;
+                  $select = mysql_query($sql);
+                  while ($rsCV = mysql_fetch_array($select))
+                  {
+
+                    ?>
+                    <option value="<?php echo($rsCV['id_modelo_veiculo']) ?>"><?php echo($rsCV['modelo']) ?></option>
+                    <?php
+                  }
+                }
+                ?>
+            </select>
+
+            <select class="select-pac" required name="slcCategoria">
+
+
+              <option selected disabled value="">Categoria</option>
+              <?php
+              if ($IdFabricant<>0)
+              {
+                  $sql = "SELECT * FROM da categoria = ".$IdFabricant;
+                  $select = mysql_query($sql);
+                  while ($rsCV = mysql_fetch_array($select))
+                  {
+
+                    ?>
+                    <option value="<?php echo($rsCV['id_modelo_veiculo']) ?>"><?php echo($rsCV['modelo']) ?></option>
+                    <?php
+                  }
+                }
+                ?>
+            </select>
+
+            <label for="txtNome" class="field-label">Nome do Produto</label>
+            <input id="txtNome" value="<?php echo($_SESSION['nome']); ?>" class="android-input input-text" type="text" name="txtNome">
+
+            <label for="txtPreco" class="field-label">Preço do Produto</label>
+            <input id="txtPreco" value="<?php echo($_SESSION['preco']); ?>" class="android-input input-text" type="text" name="txtPreco">
+
+            <label for="txtConteudo" class="field-label">Conteúdo presente na Embalagem</label>
+            <input id="txtConteudo" value="<?php echo($_SESSION['conteudo_embalagem']); ?>" class="android-input input-text" type="text" name="txtConteudo">
+
+            <select class="select-pac" required name="slcGarantia">
+              <option selected disabled value="">Garantia</option>
+              <?php
+              $sql = "SELECT * FROM tbl_cor";
+                $select = mysql_query($sql);
+                while ($rsCV = mysql_fetch_array($select))
+                {
+
+              ?>
+              <option value="<?php echo($rsCV['id_cor']) ?>"><?php echo($rsCV['cor']) ?></option>
+              <?php
+              }
+              ?>
+            </select>   
+
+            <label for="txtDescricao" class="field-label">Descrição</label>
+            <input id="txtDescricao" value="<?php echo($_SESSION['descricao']); ?>" class="android-input input-text" type="text" name="txtDescricao">
+
+            <label for="txtObs" class="field-label">Observação</label>
+            <input id="txtObs" value="<?php echo($_SESSION['observacao']); ?>" class="android-input input-text" type="text" name="txtObs">
+
         </div>
-        <form name="frmPH" method="post" action="editar_produto_home.php">
-          <div class="Container_input">
-            <div class="input">
-              <input type="text" name="txtModelo" value="<?php echo($_SESSION['id_modelo_produto']); ?>">
-            </div>
-            <div class="input">
-              <input type="text" name="txtCor" value="<?php echo($_SESSION['id_cor']); ?>">
-            </div>
-            <div class="input">
-              <input type="text" name="txtCategoria" value="<?php echo($_SESSION['id_categoria_produto']); ?>">
-            </div>
-            <div class="input">
-              <input type="text" name="txtNome" value="<?php echo($_SESSION['nome']); ?>">
-            </div>
-            <div class="input">
-              <input type="text" name="txtPreco" value="<?php echo($_SESSION['preco']); ?>">
-            </div>
-            <div class="input">
-              <input type="text" name="txtConteudo" value="<?php echo($_SESSION['conteudo_embalagem']); ?>">
-            </div>
-            <div class="input">
-              <input type="text" name="txtGarantia" value="<?php echo($_SESSION['garantia']); ?>">
-            </div>
-            <div class="input">
-              <input type="text" name="txtDescricao" value="<?php echo($_SESSION['descricao']); ?>">
-            </div>
-            <div class="input">
-              <input type="text" name="txtObs" value="<?php echo($_SESSION['observacao']); ?>">
-            </div>
-          </div>
-          <div class="input input_submit">
-            <input type="submit" name="btEditar" value="Editar">
-          </div>
-        </form>
-      </body>
-    </html>
+
+        <input class="input-submit-cad-veic" type="submit" name="btEditar" value="Editar Produto">
+
+      </form>  
+      
+    </div>
+
+    <script src="../js/jquery.js"></script>
+    <script src="../js/pac_framework.js"></script>
+
+  </body>
+
+</html>
